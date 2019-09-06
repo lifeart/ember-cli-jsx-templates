d06e6e4a9806de431c37096dadaaeceaba9f6919
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-09-07 02:56:50 +0300

    deps update

--- tests/dummy/app/templates/components/hello-piterjs.jsx
+++ tests/dummy/app/templates/components/hello-piterjs.jsx
@@ -1,6 +1,21 @@
-export function template({confName, children, showLabel}) {
-    let borderRadius = '15px';
-return <><h1>{ showLabel && <span>This is  {confName}</span> } {children}
-    <ConfLogo style={{width:'200px','backgroundColor': 'red', borderRadius}} name="[I'm another logo]" url="images/react.png" />
-    </h1></>;
+export function template({ confName, children, showLabel }) {
+  let borderRadius = "15px";
+  let size = 25*10;
+  return (
+    <>
+      <h1>
+        {showLabel && <span>This is {confName}</span>} {children}
+        <ConfLogo
+          style={{
+              width: `${size}px`, 
+              backgroundColor: "silver", 
+              border: '1px solid black',
+              borderRadius
+          }}
+          name="[I'm another logo]"
+          url="images/react.png"
+        />
+      </h1>
+    </>
+  );
 }
